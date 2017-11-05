4b383ea3d9a5c88075241d3980e1a5712cac43e4
郑付超 <hzzhengfuchao01@corp.netease.com>
2017-11-05 21:30:08 +0800

    ADD: 初始化列表

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-
+import { HttpModule } from '@angular/http'
+import { FormsModule } from '@angular/forms'
+import { NgZorroAntdModule, NZ_LOCALE, enUS } from 'ng-zorro-antd';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,9 +11,17 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    BrowserAnimationsModule,
+    NgZorroAntdModule.forRoot()
   ],
-  providers: [],
+  providers: [
+    {
+      provide: NZ_LOCALE, useValue: enUS
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
